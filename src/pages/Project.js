6b2daca8645260a2ac1d442edcea3712f3ba3826
Anthony Li <me@anli.dev>
2021-11-18 15:22:03 -0500

    Add ProjectContent

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -4,6 +4,18 @@ import { faUser, faUsers, faCalendar, faExternalLinkAlt } from '@fortawesome/fre
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { names } from "../data/names";
 
+/**
+ * @typedef {object} Project
+ * @property {string} name
+ * @property {string[]} owner
+ * @property {string} date_proposed
+ * @property {string} time
+ * @property {string} [description]
+ * @property {string} [github]
+ * @property {string} [url]
+ * @property {string} type
+ */
+
 function seed_rand(seed) {
     // https://stackoverflow.com/a/424445
     const m = 0x80000000; // 2**31;
@@ -26,6 +38,52 @@ function get_random_names(rand) {
     return [...new Set(res)];
 }
 
+/**
+ * Contents for a project.
+ * @param {Project} project project to display
+ */
+function ProjectContent(project) {
+    return (
+        <>
+            <div className="flex flex-col sm:flex-row flex-wrap sm:items-end">
+                <div>
+                    <div className="flex flex-row flex-wrap items-center space-x-2 mb-1">
+                        <h1 className="text-3xl font-bold">{project.name}</h1>
+                        <p className="italic">({project.type})</p>
+                    </div>
+                    <div className="flex flex-row items-center space-x-1">
+                        <FontAwesomeIcon icon={project.owner.length == 1 ? faUser : faUsers} />
+                        <p className="italic">{project.owner.join(", ")}</p>
+                    </div>
+                </div>
+                <div className="sm:flex-grow" />
+                <div className="flex flex-row items-center space-x-1">
+                    <FontAwesomeIcon icon={faCalendar} />
+                    <p>{project.date_proposed} {project.time}</p>
+                </div>
+            </div>
+            <hr className="my-1" />
+            <div>
+                <p className="italic">Description:</p>
+                <p>
+                    {project.description}
+                </p>
+            </div>
+            <div className="flex flex-row flex-wrap mt-1">
+                <div className="flex flex-row items-center space-x-1">
+                    <FontAwesomeIcon icon={faGithub} />
+                    <a href="https://www.youtube.com/watch?v=iik25wqIuFo" className="italic underline text-blue-500">https://example</a>
+                </div>
+                <div className="w-2" />
+                <div className="flex flex-row items-center space-x-1">
+                    <FontAwesomeIcon icon={faExternalLinkAlt} />
+                    <a href="https://www.youtube.com/watch?v=iik25wqIuFo" className="italic underline text-blue-500">https://example</a>
+                </div>
+            </div>
+        </>
+    );
+}
+
 export function Project() {
     const { id } = useParams();
     const rand = seed_rand(id);
@@ -34,46 +92,7 @@ export function Project() {
     return (
         <div className="bg-hackbca-dark-blue min-h-screen p-8 flex justify-center items-center">
             <div className="bg-white rounded shadow-lg p-8 w-192 max-w-full">
-                <div className="flex flex-col sm:flex-row flex-wrap sm:items-end">
-                    <div>
-                        <div className="flex flex-row flex-wrap items-center space-x-2 mb-1">
-                            <h1 className="text-3xl font-bold">Project {id}</h1>
-                            <p className="italic">(Software)</p>
-                        </div>
-                        <div className="flex flex-row items-center space-x-1">
-                            <FontAwesomeIcon icon={names.length == 1 ? faUser : faUsers} />
-                            <p className="italic">{names.join(", ")}</p>
-                        </div>
-                    </div>
-                    <div className="sm:flex-grow" />
-                    <div className="flex flex-row items-center space-x-1">
-                        <FontAwesomeIcon icon={faCalendar} />
-                        <p>Nov 12, 2021 14:50 EST</p>
-                    </div>
-                </div>
-                <hr className="my-1" />
-                <div>
-                    <p className="italic">Description:</p>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                        do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                        irure dolor in reprehenderit in voluptate velit esse cillum
-                        dolore eu fugiat nulla pariatur.
-                    </p>
-                </div>
-                <div className="flex flex-row flex-wrap mt-1">
-                    <div className="flex flex-row items-center space-x-1">
-                        <FontAwesomeIcon icon={faGithub} />
-                        <a href="https://www.youtube.com/watch?v=iik25wqIuFo" className="italic underline text-blue-500">https://example</a>
-                    </div>
-                    <div className="w-2" />
-                    <div className="flex flex-row items-center space-x-1">
-                        <FontAwesomeIcon icon={faExternalLinkAlt} />
-                        <a href="https://www.youtube.com/watch?v=iik25wqIuFo" className="italic underline text-blue-500">https://example</a>
-                    </div>
-                </div>
+                
             </div>
         </div>
     )
